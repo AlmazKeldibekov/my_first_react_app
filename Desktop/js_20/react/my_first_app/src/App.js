896de6f9d3cace5c7d29f3b05ed0e9ca1cd62e1a
AlmazKeldibekov <a.keldibekov.k@gmail.com>
2023-01-13 19:25:19 +0600

    Create button component

---
+++ Desktop/js_20/react/my_first_app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from './components/Button'
+import './App.css'
+
+const App = () => {
+
+  return <Test />
+}
+
+export default App;
---
+++ Desktop/js_20/react/my_first_app/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const button = () => {
+    return <div id = 'root_id' className='root_class'> 
+    <input/>
+    <button> добавить </button>
+  </div>;
+}
+
+
